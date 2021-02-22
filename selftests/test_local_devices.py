e9f4a095adc50624cbb3abea1114263cc6d21747
Karel Kočí <cynerd@email.cz>
2021-02-22 15:09:29 +0100

    selftests: check if devices are on system
    This is just test that checks that we have all required devices on host
    system. These devices are optional and additional modules are requires
    to be installed and loaded to kernel.
    
    At the moment only the /dev/ppp is tested there.

---
+++ selftests/test_local_devices.py
@@ -0,0 +1,11 @@
+import pytest
+import pathlib
+
+
+@pytest.mark.parametrize("device", [
+    "/dev/ppp",
+])
+def test_local_device(device):
+    """Check that we have local device required for testing.
+    """
+    assert pathlib.Path(device).exists()
