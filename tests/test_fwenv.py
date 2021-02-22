eb53fc8492e38529af76c893ba0b8858895118ad
Karel Kočí <cynerd@email.cz>
2021-02-22 15:20:54 +0100

    tests/test_fwenv: check if we can read uboot environment

---
+++ tests/test_fwenv.py
@@ -0,0 +1,10 @@
+"""This checks that we have correctly configured access to uboot environment using uboot-tools.
+"""
+import pytest
+
+
+def test_fw_printenv(client_board):
+    """Check that we can access uboot environment.
+    """
+    client_board.run("fw_printenv")
+    assert "Warning: Bad CRC, using default environment" not in client_board.output
