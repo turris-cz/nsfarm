d03067ea91502ab0ce5e0a642e574ab65eda9278
Karel Kočí <karel.koci@nic.cz>
2022-01-25 14:20:48 +0100

    nsfarm/toolbox/alpine: fix network_connect invalid detection
    The nc does not return "succeeded" but rather "open" now, so relying on
    exit code is much better.

--- nsfarm/toolbox/alpine.py
+++ nsfarm/toolbox/alpine.py
@@ -3,6 +3,5 @@
 
 
 def network_connect(shell, target: str, port: int, udp: bool = False):
-    """Basic netcat connection - only connects"""
-    shell.run(f"nc -vz{'u' if udp else ''} {target} {port}", None)
-    return "succeeded!" in shell.output
+    """Basic netcat connection - only connects."""
+    return shell.run(f"nc -vz{'u' if udp else ''} {target} {port}", None) == 0
